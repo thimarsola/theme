b1d4d526f90120cb2469ae20b0eb6f16c795aa09
Thiago Marsola <tmarsola94@gmail.com>
2022-08-03 09:13:06 -0300

    fix: fix the start.sh

--- start.sh
+++ start.sh
@@ -10,10 +10,7 @@ echo ""
 sudo rm -r .git
 
 echo ""
-git init
 npm install
-npx husky install
-chmod a+x .husky/commit-msg
 
 echo ""
 composer update
@@ -24,27 +21,28 @@ mv source/Config-example.php source/Config.php
 echo ""
 printf "Digite o nome do pacote: "
 read packageName
-sed -i 's/package-name/'$packageName'/g' package.json
+sed -i 's/package-name/'$packageName'/g' package.json composer.json
 
 echo ""
 printf "Digite o nome do projeto: "
 read projetName
-sed -i 's/project-name/'"$projetName"'/g' package.json
-sed -i 's/project-name/'"$projetName"'/g' composer.json
-sed -i 's/project-name/'"$projetName"'/g' README.md
-sed -i 's/project-name/'"$projetName"'/g' style.css
+sed -i 's/project-name/'"$projetName"'/g' package.json README.md style.css
 
 echo ""
 printf "Digite o nome do repositório no Github: "
 read repoName
 sed -i 's/repo-name/'$repoName'/g' package.json
+
 gh repo create $repoName --private
+git init
 git add README.md
 git commit -m "first commit"
 git branch -M main
 git remote add origin git@github.com:thimarsola/$repoName.git
 git push -u origin main
 git flow init
+npx husky install
+chmod a+x .husky/commit-msg
 
 echo ""
 printf "Digite o dominio do projeto: "
