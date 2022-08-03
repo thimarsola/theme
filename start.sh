3d3da9c9809f07a5f63644d8959783827229a8c7
Thiago Marsola <tmarsola94@gmail.com>
2022-08-03 08:52:38 -0300

    fix: fix the start.sh

--- start.sh
+++ start.sh
@@ -29,7 +29,7 @@ sed -i 's/package-name/'$packageName'/g' package.json
 echo ""
 printf "Digite o nome do projeto: "
 read projetName
-sed -i '' 's/project-name/'$projetName'/g' package.json composer.json README.md style.css
+sed -i "s/project-name/"$projetName"/g" package.json composer.json README.md style.css
 
 echo ""
 printf "Digite o nome do repositório no Github: "
