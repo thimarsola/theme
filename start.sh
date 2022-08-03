4e0f3da5627b0f43638337b7d57dc02a84dc95c1
Thiago Marsola <tmarsola94@gmail.com>
2022-08-03 08:43:56 -0300

    fix: fix the start.sh

--- start.sh
+++ start.sh
@@ -14,8 +14,6 @@ git init
 npm install
 npx husky install
 chmod a+x .husky/commit-msg
-echo ""
-git flow init
 
 echo ""
 composer update
@@ -31,20 +29,24 @@ sed -i 's/package-name/'$packageName'/g' package.json
 echo ""
 printf "Digite o nome do projeto: "
 read projetName
-sed -i 's/project-name/'$projetName'/g' package.json
-sed -i 's/project-name/'$projetName'/g' composer.json
-sed -i 's/project-name/'$projetName'/g' README.md
-sed -i 's/project-name/'$projetName'/g' style.css
+sed -i 's/project-name/'$projetName'/g' package.json composer.json README.md style.css
 
 echo ""
 printf "Digite o nome do repositório no Github: "
 read repoName
 sed -i 's/repo-name/'$repoName'/g' package.json
+gh repo create $repoName --private
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:thimarsola/$repoName.git
+git push -u origin main
+git flow init
 
 echo ""
 printf "Digite o dominio do projeto: "
 read domain
-sed -i 's/domain/'$domain'/g' package.json
+sed -i 's/domain/'$domain'/g' style.css
 
 echo ""
 echo "---------- >>> Finish Config <<< ----------"
