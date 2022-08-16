6fe1f186c74ef33aee0e5bff040d94a1db67af90
Thiago Marsola <tmarsola94@gmail.com>
2022-08-16 10:06:04 -0300

    feat: include carousel

--- assets/js/theme/functions/carousel.js
+++ assets/js/theme/functions/carousel.js
@@ -1,22 +1,27 @@
-$(function () {
-    $(".testimonial__row").owlCarousel({
-        loop: true,
-        margin: 30,
-        nav: false,
-        autoplay: true,
-        autoplayTimeout: 4000,
-        autoplayHoverPause: true,
-        autoHeight:true,
-        responsive: {
-            0: {
-                items: 1,
-            },
-            600: {
-                items: 1,
-            },
-            1000: {
-                items: 2,
-            }
-        }
-    });
+var swiper = new Swiper(".slide", {
+    slidesPerView: 3,
+    spaceBetween: 32,
+    loop: "true",
+    fade: "true",
+    grabCursos: "true",
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
 });
