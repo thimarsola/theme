f4f43629ffe58f003716ab810cfe1ee6164ba794
Thiago Marsola <tmarsola94@gmail.com>
2022-08-25 21:40:52 -0300

    feat: accordion js

--- assets/js/theme/functions/toggle.js
+++ assets/js/theme/functions/toggle.js
@@ -1,9 +1,31 @@
-$(function (){
-    $('.doubts__container__content__card__body').hide();
+/**
+ * Code example: https://github.com/Basir-PD/100-Projects-HTML-CSS-JavaScript/tree/master/7%20-%20How%20To%20Create%20an%20Accordion
+ */
 
-    $('.doubts__container__content__card__header').click(function(){
-        $(this).next().slideToggle();
-        $(this).parent().siblings().children().next().slideUp();
-        return false;
+const header = document.querySelectorAll(".accordion-header");
+const body = document.querySelectorAll(".accordion-body");
+
+header.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        body.forEach((acc) => {
+            if (
+                e.target.nextElementSibling !== acc &&
+                acc.classList.contains("active")
+            ) {
+                acc.classList.remove("active");
+                header.forEach((btn) => btn.classList.remove("active"));
+            }
+        });
+
+        const panel = btn.nextElementSibling;
+        panel.classList.toggle("active");
+        btn.classList.toggle("active");
     });
 });
+
+window.onclick = (e) => {
+    if (!e.target.matches(".accordion-header")) {
+        header.forEach((btn) => btn.classList.remove("active"));
+        body.forEach((acc) => acc.classList.remove("active"));
+    }
+};
