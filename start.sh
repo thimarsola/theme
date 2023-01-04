1acdd7cc685f18895f5c739d9b7df54df1c56cc0
Marsola <tmarsola94@gmail.com>
2023-01-04 11:31:35 -0300

    fix: fix start.sh

--- start.sh
+++ start.sh
@@ -1,51 +1,35 @@
-#!/bin/bash
-
-clear
-echo ""
-echo "---------- >>> Start Project Config <<< ----------"
-echo ""
+#!/bin/sh
+echo "---------- >>> Start Project Config <<< ----------";
+echo " "
 echo "Start project ..."
-echo ""
-
-sudo rm -r .git
-
-echo ""
-npm install
-
-echo ""
-composer update
-
-echo ""
+sudo rm -rf .git
+npm install;
+echo " "
+composer update;
 mv source/Config-example.php source/Config.php
-
-echo ""
-printf "Digite o nome do pacote(example): "
+echo " "
+echo "Digite o nome do pacote(example):"
 read packageName
-sed -i 's/package-name/'$packageName'/g' package.json composer.json
-
-echo ""
-printf "Digite o nome do projeto(Project Name): "
+sed -i "s/package-name/$packageName/g" package.json composer.json;
+echo " "
+echo "Digite o nome do projeto(Project Name):"
 read projetName
-sed -i 's/project-name/'"$projetName"'/g' package.json composer.json README.md style.css
-
-echo ""
-printf "Digite o nome do repositório no Github(Projeto-Project-Name): "
+sed -i "s/project-name/$projetName/g" package.json composer.json README.md style.css
+echo " "
+echo "Digite o nome do repositÃ³rio no Github(Projeto-Project-Name):"
 read repoName
-sed -i 's/repo-name/'$repoName'/g' package.json
-
-gh repo create $repoName --private
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:thimarsola/$repoName.git
-git push -u origin main
-git flow init
-
-echo ""
-printf "Digite o dominio do projeto: "
+sed -i "s/repo-name/$repoName/g" package.json
+echo " "
+gh repo create $repoName --private;
+git init;
+git add README.md;
+git commit -m "first commit";
+git branch -M main;
+git remote add origin git@github.com:thimarsola/$repoName.git;
+git push -u origin main;
+git flow init;
+echo " "
+echo "Digite o dominio do projeto:"
 read domain
-sed -i 's/domain/'$domain'/g' style.css README.md
-
-echo ""
+sed -i "s/domain/$domain/g" style.css README.md
 echo "---------- >>> Finish Config <<< ----------"
