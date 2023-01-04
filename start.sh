fd2404e2c210a8fe6a5f55416ea890ce4430753c
Marsola <tmarsola94@gmail.com>
2023-01-04 12:38:46 -0300

    fix: change start.sh

--- start.sh
+++ start.sh
@@ -1,22 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 echo "---------- >>> Start Project Config <<< ----------"
 echo " "
 echo "Start project ..."
-sudo rm -rf .git;
+sudo rm -rf .git
 npm install;
 echo " "
-composer update;
+composer update
 mv source/Config-example.php source/Config.php
 echo " "
-printf "Digite o nome do pacote(example):"
+echo "Digite o nome do pacote(example):"
+printf
 read packageName
 sed -i "s/package-name/$packageName/g" package.json composer.json
 echo " "
-printf "Digite o nome do projeto(Project Name):"
+echo "Digite o nome do projeto(Project Name):"
+printf
 read projetName
 sed -i "s/project-name/$projetName/g" package.json composer.json README.md style.css
 echo " "
-printf "Digite o nome do repositÃ³rio no Github(Projeto-Project-Name):"
+echo "Digite o nome do repositÃ³rio no Github(Projeto-Project-Name):"
+printf
 read repoName
 sed -i "s/repo-name/$repoName/g" package.json
 echo " "
