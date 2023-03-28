2e84b4e4d41662b2262f065db6f9b6e2d1482eb5
Marsola <tmarsola94@gmail.com>
2023-03-28 18:54:08 -0300

    wordpress standardization

--- source/Config-example.php
+++ source/Config-example.php
@@ -3,62 +3,63 @@
 /**
  * DEVELOPER
  */
-define("DEV", [
-    "name" => "Goognet Solução Digital",
-    "url" => "https://goognet.com.br",
+const DEV = array(
+    "name"   => "Goognet Solução Digital",
+    "url"    => "https://www.goognet.com.br",
     "domain" => "goognet.com.br"
-]);
+);
 
 /**
  * SITE CONFIG
  */
-define("SITE", [
-    "name" => "name",
-    "domain" => "domain.com.br",
+const SITE = array(
+    "name"   => "",
+    "domain" => "",
     "locale" => "pt_BR",
-    "lang" => "pt-BR",
-    "image" => "image.jpg",
-    "gtm" => ''
-]);
+    "lang"   => "pt-BR",
+    "image"  => "image.jpg",
+    "gtm"    => ""
+);
 
 /**
  * MAIL CONNECT
  */
-define("MAIL", [
-    "host" => "mailhog",
-    "port" => "1025",
-    "user" => null,
-    "passwd" => null,
-    "from_name" => "Website Name",
+const MAIL = array(
+    "host"       => "mailhog",
+    "port"       => "1025",
+    "user"       => null,
+    "passwd"     => null,
+    "from_name"  => "Website Name",
     "from_email" => "hello@example.com"
-]);
+);
 
 /**
  * REGION
  */
-define("REGION", [
-    "region" => "BR-SP",
-    "placename" => "S&atilde;o Caetano do Sul",
-    "lat" => "-23.632206",
-    "long" => "-46.582405"
-]);
+const REGION = array(
+    "region"    => "BR-SP",
+    "placename" => "S&atilde;o Paulo",
+    "lat"       => "-23.627606",
+    "long"      => "-46.599785"
+);
 
 /**
  * CONTACT
  */
-define("CONTACT", [
-    "whatsapp" => [
-        "number" => "",
+const CONTACT = array(
+    "whatsapp"    => [
+        "number"  => "",
         "message" => "",
     ],
-    "phone" => [
-        "",
-    ],
-    "mail" => "",
-    "address" => "",
+    "phone"       => array(),
+    "mail"        => "",
+    "address"     => "",
     "addressLink" => "",
+);
+
+const SOCIAL = array(
     "instagram" => "",
-    "facebook" => "",
-    "youtube" => null,
-    "linkedin" => null
-]);
+    "facebook"  => "",
+    "youtube"   => null,
+    "linkedin"  => null
+);
