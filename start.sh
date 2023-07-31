ce420d976d641320857dbd59fd50bfaee99ab682
Marsola <tmarsola94@gmail.com>
2023-07-30 22:49:03 -0300

    fixed the script for macOS

--- start.sh
+++ start.sh
@@ -1,37 +1,44 @@
 #!/usr/bin/env sh
-sudo rm -rf .git
+# remove git repository
+rm -rf .git
+# start configuration script
 echo "---------- >>> Start Project Config <<< ----------"
 echo " "
 echo "Start project ..."
+# run npm install
 npm install
 echo " "
+# run composer install
 composer install
 echo " "
+# install phpcodesniffer
 composer require dealerdirect/phpcodesniffer-composer-installer
 echo " "
+# copy config file
 mv source/Config-example.php source/Config.php
 echo " "
+# configure repository
 echo "Digite o nome do pacote(example-name):"
 printf ""
 read packageName
-sed -i "s/package-name/$packageName/g" package.json composer.json
+sed "s/package-name/$packageName/g" package.json composer.json
 echo " "
 echo "Digite o nome do projeto(Name):"
 printf ""
 read projetName
-sed -i "s/project-name/$projetName/g" package.json composer.json README.md style.css
+sed "s/project-name/$projetName/g" package.json composer.json README.md style.css
 echo " "
 echo "Digite o nome do repositório no Github(Projeto-Name):"
 printf ""
 read repoName
-sed -i "s/repo-name/$repoName/g" package.json README.md
+sed "s/repo-name/$repoName/g" package.json README.md
 echo " "
 gh repo create $repoName --private
 echo " "
 echo "Digite o dominio do projeto(domain.com.br):"
 printf ""
 read domain
-sed -i "s/domain/$domain/g" style.css README.md
+sed "s/domain/$domain/g" style.css README.md
 git init
 git add README.md
 git commit -m "KEY-0: start project"
@@ -43,8 +50,8 @@ npm run dev
 git add .
 git commit -m "KEY-0: wip"
 git checkout -b feature/KEY-01-initial-config
+# install husky
 npm install husky --save-dev
 npx husky install
-dos2unix .husky/commit-msg
 echo " "
 echo "---------- >>> Finish Config <<< ----------"
